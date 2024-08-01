97501565f2b37dc9e4ee8a280ba139758c1df16f
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-01 14:48:29 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Finds the floor of a number."""
+
+
+def floor(n: float) -> int:
+    """Finds the floor of a number.
+
+    Args:
+        n (float): The number to find the floor of.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return int(n)
