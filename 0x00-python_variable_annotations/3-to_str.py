86c342840c5c2858a8920491c6226b262c1168ad
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-01 14:50:21 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Floor."""
+
+def to_str(n: float) -> str:
+    """Finds the floor of a number.
+
+    Args:
+        n (float): The number to find the floor of.
+
+    Returns:
+        str: The floor of the number.
+    """
+    return str(n)
