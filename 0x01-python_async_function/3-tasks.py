15b7d376bd87ac40ca10f0b9468c002385dc0a05
MahmoudElSherbinee <mahmoudsherbine168@gmail.com>
2024-08-05 18:55:00 +0300

    fix 4 do task 4

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+"""
+Tasks 3
+Write a function (do not create an async function,
+use the regular function syntax to do this) task_wait_random
+that takes an integer max_delay and returns a asyncio.Task.
 
+"""
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,6 +2,29 @@
 
 """
 Tasks 4
+Take the code from wait_n and alter it into a new function task_wait_n.
+The code is nearly identical to wait_n except task_wait_random is being called.
 """
 
 import asyncio
+from typing import List
+# wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """ This Funcition returns a lists of delays """
+    # wait = await wait_random(max_delay)
+    task = []
+    wait = []
+
+    for i in range(n):
+        # tasks = wait_random(max_delay)
+        tasks = task_wait_random(max_delay)
+        task.append(tasks)
+
+    for tasks in asyncio.as_completed((task)):
+        delay = await tasks
+        wait.append(delay)
+
+    return wait
